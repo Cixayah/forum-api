94c2afb563739b003c98b1b56fdf137152dbade5
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-07-04 10:31:19 -0300

    CRUD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+  //Create
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+  //Read
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+  //Update
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where,
+    });
+  }
+  //Delete
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
+}
